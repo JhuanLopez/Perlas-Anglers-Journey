25bb64e960f5320b5fcec8972507aaac4abfa111
JhuanLopez <lopez.johnpatrick@ncst.edu.ph>
2025-05-14 00:41:20 +0800

    changes done

--- js/UserAccount.js
+++ js/UserAccount.js
@@ -1,7 +1,7 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
+    apiKey: "AIzaSyDEMS5xrTRv7mW9IvOquAaukWpAYJfvl3E",
     authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
+    projectId: "perlasdb-53fcd",
     storageBucket: "YOUR_STORAGE_BUCKET",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
